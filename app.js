47fa403755696abbae609fcfb03741a726bb596b
SalmanMKC <13schishti@gmail.com>
2019-12-05 15:20:37 +0000

    setting up checks for messages

--- app.js
+++ app.js
@@ -10,7 +10,13 @@ const startBot = async () => {
     try{
         await client.connect();
         console.log("Successfully connected to the bot");
-
+        client.on("PM", async({message, user}) =>{
+            if(user.ircUsername === USERNAME){
+                client.
+            }
+            if(message[0] !== "!") return;
+            
+        });
     } catch(e1){
         console.error(e1);
     }
